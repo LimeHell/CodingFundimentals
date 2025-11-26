c392ce1b391e516bebebd78b25322297097cde00
Lime Hell <judeestone@hotmail.com>
2025-11-26 10:58:13 +0000

    automating the priting for some reason

--- w3 classes learning.py
+++ w3 classes learning.py
@@ -1,22 +1,42 @@
-People = []
+class Controller:
+    def __init__(self, array, properties):
+        self.array = array
+    
+    def PrintEach(self):
+        for i in self.array:
+            print(i)
 
+# Have an array for all people
+People = []
+PeopleProperties = ["firstname", "lastname", "middlename"]
+PC = Controller(People)
+Judes = []
+JC = Controller(Judes)
+
+#Define person
 class Person:
     def __init__(self, firstname, lastname):
         People.append(self)
         self.firstname = firstname
         self.lastname = lastname
+        if firstname == "Jude":
+            Judes.append(self)
     
     def __str__(self):
         return f"{self.firstname} {self.lastname} mogs you btw"
-        
-p1 = Person("Jude", "Stone")
-p2 = Person("not Jude", "Stone")
-p3 = Person("still a", "now am i?")
 
-for i in People:
+#Some sample people 
+p1 = Person("Jude", "Stone")
+p2 = Person("Jude", "Epic")
+p3 = Person("jawline", "enjoyer")
+
+#adding a middle name to the Judes
+for i in Judes:
     i.middlename = "Gooza"
+for i in People:
     print(i.firstname, i.middlename, i.lastname)
 
+# add a middle name for 
 Person.middlename = "Geeeza"
 for i in People:
     print(i)
@@ -26,6 +46,7 @@ Jobs = []
 class Job:
     def __init__(self, JobName, Salery, QualificationReq, PossibleEmployees, Description):
         class JobName(Person):
+            
             pass
         self.Salery = Salery
         self.QualificationReq = QualificationReq
@@ -33,7 +54,7 @@ class Job:
         self.Description = Description
         Jobs.append(self)
 
-Devops = Job("£5", "Jawline", 7, "Tell people how to do their jobs well")
+Devops = Job("DevOps", "£5", "Jawline", 7, "Tell people how to do their jobs well")
 
         
     
